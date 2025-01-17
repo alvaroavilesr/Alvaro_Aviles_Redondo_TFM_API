c3acfa83db578fdc8fc1a75362c1ea08d37593e0
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-01-17 14:05:37 +0100

    Added some tests #5

--- src/main/java/es/upm/tfm/adapters/mysqldb/response/RoleResponse.java
+++ src/main/java/es/upm/tfm/adapters/mysqldb/response/RoleResponse.java
@@ -2,6 +2,8 @@ package es.upm.tfm.adapters.mysqldb.response;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -11,4 +13,13 @@ import lombok.*;
 public class RoleResponse {
     private String roleName;
     private String roleDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleResponse that = (RoleResponse) o;
+        return Objects.equals(roleName, that.roleName) &&
+                Objects.equals(roleDescription, that.roleDescription);
+    }
 }
